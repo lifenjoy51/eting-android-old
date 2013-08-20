ddbd1761de23eb94c78fbd07b7b3ad3cd2a6858f
lifenjoy51 <lifenjoy51@hotmail.com>
2013-08-21 00:17:21 +0900

    Signed-off-by: lifenjoy51 <lifenjoy51@hotmail.com>

---
+++ Eting/src/com/gif/eting/act/LockScreen.java
@@ -0,0 +1,67 @@
+package com.gif.eting.act;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.gif.eting.R;
+import com.gif.eting.ViewPagerActivity;
+
+public class LockScreen extends Activity implements OnClickListener {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.i("Eting", "onCreate");
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.lock_screen);
+
+		// 버튼 온클릭이벤트 등록
+		Button btn = (Button) findViewById(R.id.lockScreenButton);
+		btn.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view) {
+		Log.i("LickScreenOnClick", String.valueOf(view.getId()));
+		// 암호확인 버튼 클릭시
+		if (view.getId() == R.id.lockScreenButton) {
+			this.checkPassword();
+		}
+
+	}
+
+	private void checkPassword() {
+		boolean isValid = false;
+		EditText et = (EditText) findViewById(R.id.lockScreenPassword); // 입력암호
+		String pw = et.getText().toString();
+
+		Log.i("password", pw);
+
+		// 암호체크 임시로직
+		if (pw.equals("1234")) {
+			isValid = true;
+		}
+
+		// 암호 성공/실패 분기처리
+		if (isValid) {
+			Intent intent = new Intent(LockScreen.this,
+					ViewPagerActivity.class);
+			startActivity(intent);
+			finish();
+		} else {
+			// 비밀번호 틀렸을때
+			Toast toast = Toast.makeText(this, "비밀번호가 맞지 않습니다.",
+					Toast.LENGTH_SHORT);
+			toast.show();
+
+			et.setText(""); // 암호입력필드 초기화
+		}
+	}
+
+}
---
+++ Eting/src/com/gif/eting/act/WriteMyStory.java
@@ -0,0 +1,75 @@
+package com.gif.eting.act;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.gif.eting.R;
+import com.gif.eting.ReadMyStoryActivity;
+import com.gif.eting.svc.StoryService;
+import com.gif.eting.util.ServiceCompleteListener;
+
+public class WriteMyStory extends Activity implements OnClickListener{
+	
+	private StoryService storyService;
+	private ProgressDialog progressDialog;
+	private Context context;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.write_story);
+		this.context = getApplicationContext();
+
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.send_story_btn:
+			sendAndSaveStory();
+			break;
+		}
+	}
+	
+	private void sendAndSaveStory(){
+		
+		EditText et = (EditText) findViewById(R.id.story_content);
+		String content = et.getText().toString();	//이야기 내용
+		
+		//전송상태 나타냄
+		progressDialog = ProgressDialog.show(this, "", getResources().getString(R.string.app_name), true, true);
+
+		//StoryService초기화
+		storyService = new StoryService(this.getApplicationContext());
+		storyService.saveStoryToServer(content, new AfterSendAndSaveStory()); // 서버에 이야기저장, 파라미터로 콜백함수 넘김
+	}
+	
+	public class AfterSendAndSaveStory implements ServiceCompleteListener<String>{
+
+		@Override
+		public void onServiceComplete(String result) {
+			Log.i("onTaskComplete", result);
+
+			if (progressDialog != null)
+				progressDialog.dismiss();
+
+			Toast toast = Toast.makeText(context, "이야기가 전송되었습니다", Toast.LENGTH_SHORT);
+			toast.show();
+
+			// 내 이야기 읽기 화면으로 이동
+			startActivity(new Intent(context, ReadMyStoryActivity.class));
+			
+		}
+	}
+
+}
