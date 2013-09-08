f7ef79ae3301446ea2a61f85a7bb04a94b10cd73
lifenjoy51 <lifenjoy51@hotmail.com>
2013-09-09 07:42:45 +0900

    GCM added

---
+++ Eting/src/com/gif/eting/svc/gcm/GcmIntentService.java
@@ -0,0 +1,110 @@
+package com.gif.eting.svc.gcm;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.gif.eting.act.MainViewPagerActivity;
+import com.gif.eting.svc.task.CheckStampedStoryTask;
+import com.gif.etingdev.R;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+/**
+ * This {@code IntentService} does the actual handling of the GCM message.
+ * {@code GcmBroadcastReceiver} (a {@code WakefulBroadcastReceiver}) holds a
+ * partial wake lock for this service while the service does its work. When the
+ * service is finished, it calls {@code completeWakefulIntent()} to release the
+ * wake lock.
+ */
+public class GcmIntentService extends IntentService {
+    public static final int NOTIFICATION_ID = Notification.FLAG_AUTO_CANCEL;
+    private NotificationManager mNotificationManager;
+    NotificationCompat.Builder builder;
+
+    public GcmIntentService() {
+        super("GcmIntentService");
+    }
+    public static final String TAG = "GCM";
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
+
+        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
+            /*
+             * Filter messages based on message type. Since it is likely that GCM will be
+             * extended in the future with new message types, just ignore any message types you're
+             * not interested in, or that you don't recognize.
+             */
+            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                sendNotification("Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+                sendNotification("Deleted messages on server: " + extras.toString());
+            // If it's a regular GCM message, do some work.
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+            	/**
+            	 * 메세지받아오면
+            	 */            	
+                Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
+                // Post notification of received message.
+                //TODO 환경설정값에 따라서 메세지를 보여줌
+                String storyId = extras.getString("story_id");
+                sendNotification(storyId);
+                Log.i(TAG, "Received: " + extras.toString());
+            }
+        }
+        // Release the wake lock provided by the WakefulBroadcastReceiver.
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    // Put the message into a notification and post it.
+    // This is just one simple example of what you might choose to do with
+    // a GCM message.
+    private void sendNotification(String storyId) {
+    	Log.i("sendNotification", storyId);
+    	
+    	/**
+		 * 스탬프찍힌 이야기 리스트 받아오기
+		 * 
+		 * execute의 파라미터가 실제 넘겨줄 자료들.
+		 * parameter[0] = this. Context.
+		 */
+		new CheckStampedStoryTask(null).execute(this);
+		
+        mNotificationManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent intent =new Intent(this, MainViewPagerActivity.class);
+        intent.putExtra("GCM", true);
+        intent.putExtra("storyId", storyId);
+        
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+        .setSmallIcon(R.drawable.ic_launcher)
+        .setContentTitle("Eting")
+        .setStyle(new NotificationCompat.BigTextStyle()
+        .bigText("Eting!"))
+        .setContentText("Eting!!")
+        .setAutoCancel(true);
+        
+        //TODO 진동기능 설정값으로 줘야하나?
+        mBuilder.setVibrate(new long[] {100, 100, 200, 200});
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+        
+    }
+}
---
+++ Eting/src/com/gif/eting/svc/task/RegistrationTask.java
@@ -0,0 +1,54 @@
+package com.gif.eting.svc.task;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.gif.eting.util.HttpUtil;
+import com.gif.eting.util.Installation;
+import com.gif.eting.util.Util;
+
+/**
+ * 작성한 이야기를 서버에 전송하는 작업
+ * AsyncTask를 상속받았기에 Main쓰레드와 별도로 실행된다.
+ * 
+ * @author lifenjoy51
+ *
+ */
+public class RegistrationTask extends AsyncTask<Object, String, String> {
+	/**
+	 * Context
+	 */
+	private Context context;
+	
+	/**
+	 * @param callback
+	 */
+	public RegistrationTask(){
+	}
+
+	/**
+	 * 실제 실행되는 부분
+	 */
+	@Override
+	protected String doInBackground(Object... params) {
+
+			String regId = (String) params[0]; 	//파라미터 첫번째값 regId.
+			this.context = (Context) params[1];	//파라미터 두번째값 context
+			String phoneId = Installation.id(context);	//기기 고유값
+			String param = "phone_id=" + phoneId +"&reg_id=" +regId;	//서버에 전송할 파라미터 조립
+			String urlStr = Util.serverContext+"/registration";
+			
+			return HttpUtil.doPost(urlStr, param);	//Http전송
+	}	
+	
+	/**
+	 * 작업이 끝나면 자동으로 실행된다.
+	 */
+	@Override
+	protected void onPostExecute(String result) {
+
+		Log.i("json response", result);	//응답 확인
+	}
+
+}
