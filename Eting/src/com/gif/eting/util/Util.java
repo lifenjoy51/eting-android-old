906af95c052ee94972dd9d7a7f6934cdc1a412be
lifenjoy51 <lifenjoy51@gmail.com>
2014-01-10 04:52:43 +0900

    ver 1.0.6

--- Eting/src/com/gif/eting/util/Util.java
+++ Eting/src/com/gif/eting/util/Util.java
@@ -18,8 +18,8 @@ import com.gif.eting.R;
  * 
  */
 public class Util {
-	//public static String serverContext = "http://eting.cafe24.com/eting"; // 서버
-	 public static String serverContext = "http://192.168.123.100:8080/eting";
+	public static String serverContext = "http://eting.cafe24.com/eting"; // 서버
+	//public static String serverContext = "http://192.168.123.100:8080/eting";
 	// //로컬
 	public static int mainRatio = 82;
 	public static long fps = 10;
